1ae3ff273f23d0dd22b4cc129690be4525a2be30
Forshteven <geocinds@mail.ru>
2024-09-20 15:44:00 +0300

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,7 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+        def go_to(new_flor):
+            
